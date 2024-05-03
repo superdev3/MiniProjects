362d699da148d20c198e1910c0a341b26fa33712
Super /dev <168540182+superdev3@users.noreply.github.com>
2024-05-03 14:17:17 +0200

    Small refactoring of FileSorter.py
    Updated/Refactored FileSorter.py in the Python Branch

--- FileSorter.py
+++ FileSorter.py
@@ -1,9 +1,14 @@
-from rich import print, os
-res = []
+from rich import print
+import os
+
+FileArray = []
 for path in os.listdir(fr"{os.getcwd()}"): 
-    if os.path.isfile(os.path.join(fr"{os.getcwd()}", path)): res.append(path)
+    if os.path.isfile(os.path.join(fr"{os.getcwd()}", path)): 
+        FileArray.append(path)
 try:
     for i in range(0, len(res)):
-        if "." not in res[i] or res[i] == str(__file__): continue
-        os.rename(str(res[i]), f"file_{i}.{str(res[i]).split('.')[1]}")
-except Exception as e:print(f"[red]{e}[/red]")
+        if "." not in FileArray[i] or FileArray[i] == str(__file__): 
+            continue
+        os.rename(str(FileArray[i]), f"file_{i}.{str(FileArray[i]).split('.')[1]}")
+except Exception as exception:
+    print(f"[red]{exception}[/red]")
