129458ed5c3083b9c2d92bd50d4a75ff5c3b8bc7
Super /dev <168540182+superdev3@users.noreply.github.com>
2024-05-03 14:12:58 +0200

    Created SpellingCheck.py
    Created the file SpellingCheck.py in the Python Branch

---
+++ SpellingCheck.py
@@ -0,0 +1,31 @@
+from english_words import get_english_words_set
+from rich import print
+
+wordList = get_english_words_set(['web2'], lower=True)
+
+def CheckWords(argWordList: str) -> list[str]:
+    """Checks if words are correct by looping through provided string and checking if the lowered string is in the Standard English Dictionary"""
+    
+    checked = []
+    
+    for word in argWordList.split():
+        if word.casefold() in wordList:
+            checked.append(f"[green]{word}[/green]")
+        else:
+            checked.append(f"[red]{word}[/red]")
+
+    return checked
+
+# Main Loop
+while True:
+    userInput = input("Please type in a sentence and press enter to check: ")
+    
+    checkedWorldList = CheckWords(userInput)
+    
+    consoleOutput = checkedWorldList[0]
+    
+    # Turns List to string
+    for iteration in range(0, len(checkedWorldList)):
+        consoleOutput = consoleOutput + " " + checkedWorldList[iteration]
+
+    print(consoleOutput)
