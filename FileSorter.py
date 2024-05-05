bb8f87cabbaac82c806df8f876f60dda13a97b32
Super /dev <168540182+superdev3@users.noreply.github.com>
2024-05-05 20:31:54 +0200

    Updated FileSorter.py
    Reformated code in FileSorter.py

--- FileSorter.py
+++ FileSorter.py
@@ -2,13 +2,18 @@ from rich import print
 import os
 
 FileArray = []
+
 for path in os.listdir(fr"{os.getcwd()}"): 
     if os.path.isfile(os.path.join(fr"{os.getcwd()}", path)): 
         FileArray.append(path)
 try:
-    for i in range(0, len(res)):
+    for i in range(0, len(FileArray)):
         if "." not in FileArray[i] or FileArray[i] == str(__file__): 
             continue
-        os.rename(str(FileArray[i]), f"file_{i}.{str(FileArray[i]).split('.')[1]}")
+        
+        new_file_name = f"File-{i+1}.{str(FileArray[i]).split('.')[1]}"
+        os.rename(str(FileArray[i]), new_file_name)
+        print(f"Renamed {FileArray[i]} -> {new_file_name}")
+        
 except Exception as exception:
     print(f"[red]{exception}[/red]")
