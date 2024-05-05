d7407bf3dde88200dfd89288ff820ebf59d7fdf6
Super /dev <168540182+superdev3@users.noreply.github.com>
2024-05-05 21:13:36 +0200

    Created IpGeoLocator.py
    Created IpGeoLocator.py in the Main Branch

---
+++ IpGeoLocator.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+while (True):
+    ip_address = input("Ip Address to trace: ")
+    request = requests.get(f"https://ipinfo.io/{ip_address}/json")
+
+    match (request.status_code):
+        case (200):
+            request_json = json.loads(request.content)
+            print(f"City: {request_json['city']}")
+            print(f"Region: {request_json['region']}")
+            print(f"Country: {request_json['country']}")
+            print(f"Location: {request_json['loc']}")
+            print(f"Timezone: {request_json['timezone']}")
+            print(f"ASN: {request_json['org']}")
+            print(f"Google Maps: https://google.com/maps/?q={request_json['loc']}")
+            
+        case _:
+            print("[Error] retrieving Ip Information")
+            continue
