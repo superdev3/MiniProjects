ba973ac071f694ba95c10f410005eece210b9832
Super /dev <168540182+superdev3@users.noreply.github.com>
2024-06-08 09:39:08 +0200

    Updated PhoneNumberTracer.py to have fail safe

--- PhoneNumberTracer.py
+++ PhoneNumberTracer.py
@@ -4,6 +4,11 @@ from phonenumbers.geocoder import description_for_number
 
 while True:
     user_input = input("Phone number to trace: ")
-    phone_number = parse(user_input)
     
-    print(description_for_number(phone_number, "eu"))
+    try:
+        phone_number = parse(user_input)
+        print(description_for_number(phone_number, "eu"))
+        
+    except Exception:
+        print("Invalid phone number")
+        continue
